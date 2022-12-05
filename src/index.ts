4f9e9a260d62f245c1b2ce1ebcf67d0f37711712
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2022-12-05 02:02:57 +0000

    feat: getting all games data

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,49 @@
 import puppeteer from 'puppeteer';
-
-const parseClassListAsSelectorQuery = (classList: string) => `.${classList.split(' ').join('.')}`
+import { Game } from './models/game';
 
 async function gettingAllTheGamesData() {
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto('https://www.google.com/search?q=dolar+para+real&rlz=1C1GCEB_enBR1007BR1007&oq=dolar+para+real&aqs=chrome..69i57j0i271l2.2578j0j4&sourceid=chrome&ie=UTF-8')
+    await page.goto('https://wiki.supercombo.gg/')
     
     const resultElement = await page.evaluate(() => {
-        const query = parseClassListAsSelectorQuery('wikitable sortable jquery-tablesorter')
-    })
+        const array = Array.from(document.querySelectorAll('table'))
+        const gamesTable = array[1]
+        const games = Array.from(gamesTable.querySelectorAll('tr'))
+        const dataTitles = games.shift()
+        
+        const getAllCellsFromTableRow = (tableRow: HTMLTableRowElement) => Array.from(tableRow.querySelectorAll('td'))
 
-    await page.screenshot({path: 'foto.png'})
+        return games
+            .filter(game => {
+                const convertCompletionImageToPercentage = (alt: string) => Number(alt.replace(/\D/g, ''))
+                const completionImageTag = game.querySelectorAll('td')[4].childNodes[0].childNodes[0] as HTMLImageElement
+
+                const completionpercentage = convertCompletionImageToPercentage(completionImageTag.alt)
+                const validGame = completionpercentage > 20
+
+                return validGame
+            })
+            .filter(game => {
+                const data = getAllCellsFromTableRow(game)
+                const [gameNameTd] = data
+                
+                const [gameName] = Array.from(gameNameTd.childNodes) as HTMLElement[]
+                const onlyGamesThatAreNotDepricated = gameName.tagName === 'A'
+
+                return onlyGamesThatAreNotDepricated
+            })
+            .map(game => {
+                const dataCells = getAllCellsFromTableRow(game)
+                const [name,, year, developer] = dataCells.map(cell => cell.innerText)                
+
+                return {name, year, developer} as Game
+            })
+
+        
+    })
 
     await browser.close()
 
---
+++ src/models/game.ts
@@ -0,0 +1,13 @@
+export class Game {
+
+    name: string
+    year: string
+    developer: string
+
+    constructor(name: string, year: string, developer: string) {
+        this.name = name
+        this.year = year
+        this.developer = developer
+    }
+
+}
